75bb5dc55e31ef8d8e4c537b0dabd5002ddb7135
Jared <jds485@cornell.edu>
2018-05-31 14:30:55 -0400

    Outline of the data analysis for the PAD ice jam flood frequency analysis

---
+++ PAD_FloodAnalysis.R
@@ -0,0 +1,16 @@
+#Statistical Analysis for the Comment on Beltaos (2018)
+#Code Authors: Jared D. Smith (jds485@cornell.edu)
+#            : Jonathan Lamontagne (Jonathan.Lamontagne@tufts.edu)
+
+#Blurb on sections in the code----
+
+#Set working directory----
+setwd()
+
+#Load libraries----
+
+#Binomial exact tests----
+
+# Moving window analysis----
+
+#Replication of Beltaos' regression analysis tests----
