3fed6eeb2461f65a2bcaf19862e0276c66585ea4
Jared <jds485@cornell.edu>
2018-06-08 00:42:42 -0400

    Added regression diagnostic plot. Modified cumulative flood plot to color where Beltaos did not have data. Updated regression comparison for Beltaos study based on his dataset.

--- PAD_FloodAnalysis.R
+++ PAD_FloodAnalysis.R
@@ -7,33 +7,56 @@
 #Set working directory----
 setwd("C:\\Users\\jsmif\\Documents\\Cornell\\Research\\Publications\\CountRegressionIceJams\\DataAnalysis\\PAD_IceJamFloods")
 
+#Load libraries----
+library(Hmisc)
+
 #Load data----
 #All flood data
 Floods = read.csv(file = 'FloodData.csv', stringsAsFactors = FALSE)
-#Select only moderate (M) and extreme (X) floods (scale values of 2 and 3)
-MX = Floods[,c('YEAR', 'Floods23only')]
-
-#FIXME: remove years without flood data? currently using na.rm and is.na to check for NA data (no flood record) years
+#Select only moderate (M) and extreme (X) floods (scale values of 2 and 3), and Beltaos floods
+#NOTE: JDS added in 0s for missing catalog data based on sediment cores in Wolfe (2006). Years 1927 and 1928 were given 1s because of flooding at PAD54.
+MX = Floods[,c('YEAR', 'Floods23only', 'Beltaos')]
 
 #Compute cumulative number of floods for replication of Beltaos
 MX$FldSum = 0
+MX$BeltSum = 0
 for (i in 1:nrow(MX)){
   MX$FldSum[i] = sum(MX$Floods23only[seq(1,i,1)], na.rm = TRUE)
+  MX$BeltSum[i] = sum(MX$Beltaos[seq(1,i,1)], na.rm = TRUE)
 }
-#Because Beltaos started in 1900, need to adjust the cumulative number by the number observed in 1900
-MX$FldSum = MX$FldSum - MX$FldSum[MX$YEAR == 1900]
-
-#FIXME: It seems like Beltos' dataset is different than the one we are working with. Not continuing with this analysis for now.
+rm(i)
+#Because Beltaos started in 1900 at a cumulative flood count of 2, need to adjust the cumulative number by the number observed in 1900, plus 2
+MX$FldSum = MX$FldSum - MX$FldSum[MX$YEAR == 1900] + 2
+#Also need to add one to Beltaos cumulative (must have considered a flood before 1900 but did not show where it is)
+MX$BeltSum[MX$YEAR >= 1900] = MX$BeltSum[MX$YEAR >= 1900] + 1
 
 #Plot of flood data----
 png('CumulativeFloods.png', res = 300, units = 'in', width = 6, height = 6)
 par(mar = c(5,5,3,1), xaxs = 'i', yaxs = 'i')
-plot(MX$YEAR, MX$FldSum, 
-     ylim = c(-8,23), xlim = c(MX$YEAR[1], max(MX$YEAR)),
-     xlab = 'Year', ylab = 'Cumulative Number of Floods Since 1900',
+#Plot filler data: Both no floods
+plot(MX$YEAR[MX$Beltaos == 0 & MX$Floods23only == 0], MX$FldSum[MX$Beltaos == 0 & MX$Floods23only == 0], pch = 1,
+     ylim = c(-6,28), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+     xlab = '', ylab = '', axes = FALSE)
+
+#Plot Floods not considered by Beltaos
+par(new = TRUE)
+plot(MX$YEAR[MX$Beltaos == 0 & MX$Floods23only == 1], MX$FldSum[MX$Beltaos == 0 & MX$Floods23only == 1], col = 'red', pch = 16,
+     ylim = c(-6,28), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+     xlab = 'Year', ylab = 'Cumulative Number of Floods Since 1888',
      cex.axis = 1.5, cex.lab = 1.5)
+#Plot all other floods considered in both studies
+par(new = TRUE)
+plot(MX$YEAR[MX$Beltaos == 1 & MX$Floods23only == 1], MX$FldSum[MX$Beltaos == 1 & MX$Floods23only == 1], col = 'blue', pch = 16,
+     ylim = c(-6,28), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+     xlab = '', ylab = '', axes = FALSE)
+
+
 #Polygon for dam filling years
-polygon(x = c(1968, 1968, 1971, 1971), y = c(-8, 23, 23, -8), col = 'blue', density = 0, lwd = 2)
+polygon(x = c(1968, 1968, 1971, 1971), y = c(-10, 28, 28, -10), col = 'grey', density = 0, lwd = 2)
+
+minor.tick(nx = 5, ny = 5, tick.ratio = 0.5)
+legend('topleft', legend = c('Flood Not in Beltaos (2018)', 'Flood in Beltaos (2018)', 'No Flood', 'Reservoir Filling'), pch = c(16,16,1,NA), lty = c(NA,NA,NA,1), lwd = 1, col = c('red', 'blue', 'black', 'grey'), cex = 1.2)
+
 dev.off()
 
 #Pacific Decadal Osciallation indices
@@ -130,26 +153,26 @@ axis(side = 2, at = seq(0.1,0.9,0.2), labels = FALSE)
 lines(x = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), y = c(0.05, 0.05), lty = 2, lwd = 2)
 
 #Polygon for dam filling years
-polygon(x = c(1968, 1968, 1971, 1971), y = c(0, 1, 1, 0), col = 'blue', density = 0, lwd = 2)
+polygon(x = c(1968, 1968, 1971, 1971), y = c(0, 1, 1, 0), col = 'grey', density = 0, lwd = 2)
 
 par(new = TRUE)
 #Plot of the floods to show where they exist
 plot(x = MX$YEAR[MX$Floods23only == 1], y = rep(0.01,length(MX$YEAR[MX$Floods23only == 1])), 
-     pch = 1, col = 'red',
+     pch = 1, col = 'purple',
      xlim = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), ylim = c(0, 1),
      xlab = '', ylab = '', main = '', axes = FALSE)
 
-par(new = TRUE)
-#plot of the no data years to show where no data exist
-plot(x = MX$YEAR[is.na(MX$Floods23only)], y = rep(0.01,length(MX$YEAR[is.na(MX$Floods23only)])), 
-     pch = 'x', col = 'grey',
-     xlim = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), ylim = c(0, 1),
-     xlab = '', ylab = '', main = '', axes = FALSE)
+#par(new = TRUE)
+#plot of the no data years to show where no data exist - Filled in with stratigraphic column
+#plot(x = MX$YEAR[is.na(MX$Floods23only)], y = rep(0.01,length(MX$YEAR[is.na(MX$Floods23only)])), 
+#     pch = 'x', col = 'grey',
+#     xlim = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), ylim = c(0, 1),
+#     xlab = '', ylab = '', main = '', axes = FALSE)
 
 #Polygon for base years
 polygon(x = c(MX$YEAR[1], MX$YEAR[1], MX$YEAR[size], MX$YEAR[size]), y = c(0, 0.1, 0.1, 0), col = 'red', density = 0, lwd = 2)
 
-legend('topleft', legend = c('p-values', 'Floods', 'No Data', 'Dam filling', 'Base Years', '0.05 sig. level'), pch = c(16,1,4,22,22,NA), lty = c(1,NA,NA,NA,NA,2), lwd = 2, col = c('black', 'red', 'grey', 'blue','red', 'black'), cex = 1.2)
+legend('topleft', legend = c('p-values', 'Floods', 'Dam filling', 'Base Years', '0.05 sig. level'), pch = c(16,1,22,22,NA), lty = c(1,NA,NA,NA,2), lwd = 2, col = c('black', 'purple', 'grey', 'red', 'black'), cex = 1.2)
 
 dev.off()
 
@@ -169,7 +192,7 @@ for (i in 1:length(GeomProb)){
   #Compute the upper tail from geometric distribution for waiting at least x years for next flood given pbase probability in any year
   GeomProb[i] = 1 - pgeom(q = (x-2), prob = pbase)
 }
-rm(x)
+rm(x,i)
 
 png('MovingWindowGeometricWaitingTime.png', res = 300, width = 8, height = 8, units = 'in')
 par(mar = c(4,6,4,1), xaxs = 'i', yaxs = 'i')
@@ -186,56 +209,70 @@ axis(side = 2, at = seq(0.1,0.9,0.2), labels = FALSE)
 lines(x = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), y = c(0.05, 0.05), lty = 2, lwd = 2)
 
 #Polygon for dam filling years
-polygon(x = c(1968, 1968, 1971, 1971), y = c(0, 1, 1, 0), col = 'blue', density = 0, lwd = 2)
+polygon(x = c(1968, 1968, 1971, 1971), y = c(0, 1, 1, 0), col = 'grey', density = 0, lwd = 2)
 
 par(new = TRUE)
 #Plot of the floods to show where they exist
 plot(x = MX$YEAR[MX$Floods23only == 1], y = rep(0.01,length(MX$YEAR[MX$Floods23only == 1])), 
-     pch = 1, col = 'red',
+     pch = 1, col = 'purple',
      xlim = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), ylim = c(0, 1),
      xlab = '', ylab = '', main = '', axes = FALSE)
 
-par(new = TRUE)
+#par(new = TRUE)
 #plot of the no data years to show where no data exist
-plot(x = MX$YEAR[is.na(MX$Floods23only)], y = rep(0.01,length(MX$YEAR[is.na(MX$Floods23only)])), 
-     pch = 'x', col = 'grey',
-     xlim = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), ylim = c(0, 1),
-     xlab = '', ylab = '', main = '', axes = FALSE)
+#plot(x = MX$YEAR[is.na(MX$Floods23only)], y = rep(0.01,length(MX$YEAR[is.na(MX$Floods23only)])), 
+#     pch = 'x', col = 'grey',
+#     xlim = c(MX$YEAR[1], MX$YEAR[nrow(MX)]), ylim = c(0, 1),
+#     xlab = '', ylab = '', main = '', axes = FALSE)
 
 #Polygon for base years
 polygon(x = c(MX$YEAR[1], MX$YEAR[1], MX$YEAR[size], MX$YEAR[size]), y = c(0, 0.1, 0.1, 0), col = 'red', density = 0, lwd = 2)
 
-legend('topleft', legend = c('Probabilities', 'Floods', 'No Data', 'Dam filling', 'Base Years', '0.05 sig. level'), pch = c(16,1,4,22,22,NA), lty = c(NA,NA,NA,NA,NA,2), lwd = 2, col = c('black', 'red', 'grey', 'blue','red', 'black'), cex = 1.2)
+legend('topleft', legend = c('Probabilities', 'Floods', 'Dam filling', 'Base Years', '0.05 sig. level'), pch = c(16,1,22,22,NA), lty = c(NA,NA,NA,NA,2), lwd = 2, col = c('black', 'purple', 'grey','red', 'black'), cex = 1.2)
 
 dev.off()
 
 #Replication of Beltaos' regression analysis tests----
 # Years 1900 - 1967
-lm1900 = lm(MX[MX$YEAR >= 1900 & MX$YEAR <= 1967, 'FldSum'] ~ MX[MX$YEAR >= 1900 & MX$YEAR <= 1967, 'YEAR'])
+lm1900 = lm(MX[MX$YEAR >= 1900 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 1900 & MX$YEAR <= 1967, 'YEAR'])
 # Years 1910 - 1967
-lm1910 = lm(MX[MX$YEAR >= 1910 & MX$YEAR <= 1967, 'FldSum'] ~ MX[MX$YEAR >= 1910 & MX$YEAR <= 1967, 'YEAR'])
+lm1910 = lm(MX[MX$YEAR >= 1910 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 1910 & MX$YEAR <= 1967, 'YEAR'])
 # Years 1922 - 1967
-lm1922 = lm(MX[MX$YEAR >= 1922 & MX$YEAR <= 1967, 'FldSum'] ~ MX[MX$YEAR >= 1922 & MX$YEAR <= 1967, 'YEAR'])
+lm1922 = lm(MX[MX$YEAR >= 1922 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 1922 & MX$YEAR <= 1967, 'YEAR'])
 # Years 1930 - 1967
-lm1930 = lm(MX[MX$YEAR >= 1930 & MX$YEAR <= 1967, 'FldSum'] ~ MX[MX$YEAR >= 1930 & MX$YEAR <= 1967, 'YEAR'])
+lm1930 = lm(MX[MX$YEAR >= 1930 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 1930 & MX$YEAR <= 1967, 'YEAR'])
 # Years 1940 - 1967
-lm1940 = lm(MX[MX$YEAR >= 1940 & MX$YEAR <= 1967, 'FldSum'] ~ MX[MX$YEAR >= 1940 & MX$YEAR <= 1967, 'YEAR'])
+lm1940 = lm(MX[MX$YEAR >= 1940 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 1940 & MX$YEAR <= 1967, 'YEAR'])
 # Years 1950 - 1967
-lm1950 = lm(MX[MX$YEAR >= 1950 & MX$YEAR <= 1967, 'FldSum'] ~ MX[MX$YEAR >= 1950 & MX$YEAR <= 1967, 'YEAR'])
+lm1950 = lm(MX[MX$YEAR >= 1950 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 1950 & MX$YEAR <= 1967, 'YEAR'])
 # Post-dam years: 1971 - 2017
-lmPD = lm(MX[MX$YEAR >= 1971, 'FldSum'] ~ MX[MX$YEAR >= 1971, 'YEAR'])
+lmPD = lm(MX[MX$YEAR >= 1971, 'BeltSum'] ~ MX[MX$YEAR >= 1971, 'YEAR'])
 
 #Regression diagnostic plots - concerns about heteroskedasticity and normality. Showing one example.
 plot(lm1900)
 
+#Regression diagnostics plots
+png('BeltaosRegressionDiagnostics.png', res = 300, units = 'in', width = 8, height = 6)
+layout(cbind(1,2))
+par(mar = c(4,5,3,1), xaxs = 'i', yaxs = 'i')
+#Normal QQ plot for residuals
+qqnorm(lmPD$residuals, ylim = c(-1.5,1.5), xlim = c(-2.5,2.5), main = 'Regression for 1971 - 2018 \n Normal Q-Q Plot', cex.lab = 1.5, cex.axis = 1.5)
+qqline(lmPD$residuals)
+
+#Residuals vs. Fitted Y Values
+plot(lmPD$fitted.values, lmPD$residuals, type = 'p', xlim = c(13,17), ylim = c(-1.5,1.5), cex.lab = 1.5, cex.axis = 1.5,
+     xlab = 'Fitted Y Values', ylab = 'Regression Residuals', main = 'Regression for 1971 - 2018 \n Residuals vs. Fitted Y Values')
+lines(c(10,20), c(0,0), lty = 2)
+dev.off()
+
 png('BeltosRegressionCheck.png', res = 300, units = 'in', width = 7, height = 7)
-par(mar = c(4,4,3,1), xaxs = 'i', yaxs = 'i')
-plot(MX$YEAR, MX$FldSum, 
+par(mar = c(4,5,3,1), xaxs = 'i', yaxs = 'i')
+plot(MX$YEAR, MX$BeltSum, 
      ylim = c(-1,23), xlim = c(1900, max(MX$YEAR)),
-     xlab = 'Year', ylab = 'Cumulative Number of Floods',
+     xlab = 'Year', ylab = 'Beltaos (2018) Cumulative Number of Floods',
      cex.axis = 1.5, cex.lab = 1.5)
 #Polygon for dam filling years
-polygon(x = c(1968, 1968, 1971, 1971), y = c(-1, 31, 31, -1), col = 'blue', density = 0, lwd = 2)
+polygon(x = c(1968, 1968, 1971, 1971), y = c(-1, 31, 31, -1), col = 'grey', density = 0, lwd = 2)
 
 #Add regression lines
 abline(lm1900, col = 'red')
@@ -262,3 +299,7 @@ plot(x = MX$YEAR[MX$Floods23only == 1], y = rep(0.5,length(MX$YEAR[MX$Floods23on
      pch = 1,
      xlim = c(1856,1990), ylim = c(-0.5, 0.5),
      xlab = '', ylab = '', main = '', axes = FALSE)
+
+#AR Model----
+
+#Block Bootstrapping----
