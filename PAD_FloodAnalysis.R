f53bcb530e329b70ef9f0f3e3302f192c5e67921
Jared <jds485@cornell.edu>
2018-06-08 17:03:43 -0400

    Added AR models and block bootstrapping. Block bootstrapping image added.

--- PAD_FloodAnalysis.R
+++ PAD_FloodAnalysis.R
@@ -18,19 +18,18 @@ Floods = read.csv(file = 'FloodData.csv', stringsAsFactors = FALSE)
 MX = Floods[,c('YEAR', 'Floods23only', 'Beltaos')]
 
 #Compute cumulative number of floods for replication of Beltaos
-MX$FldSum = 0
-MX$BeltSum = 0
-for (i in 1:nrow(MX)){
-  MX$FldSum[i] = sum(MX$Floods23only[seq(1,i,1)], na.rm = TRUE)
-  MX$BeltSum[i] = sum(MX$Beltaos[seq(1,i,1)], na.rm = TRUE)
-}
-rm(i)
+MX$FldSum = cumsum(MX$Floods23only)
+MX$BeltSum = cumsum(MX$Beltaos)
 #Because Beltaos started in 1900 at a cumulative flood count of 2, need to adjust the cumulative number by the number observed in 1900, plus 2
 MX$FldSum = MX$FldSum - MX$FldSum[MX$YEAR == 1900] + 2
 #Also need to add one to Beltaos cumulative (must have considered a flood before 1900 but did not show where it is)
 MX$BeltSum[MX$YEAR >= 1900] = MX$BeltSum[MX$YEAR >= 1900] + 1
 
 #Plot of flood data----
+#Data missing from Beltaos include:
+#pre 1948 - RCM data, except for 1927/28 that uses Wolfe et al. (2006) stratigraphic data.
+#1948 - present WBNP data
+
 png('CumulativeFloods.png', res = 300, units = 'in', width = 6, height = 6)
 par(mar = c(5,5,3,1), xaxs = 'i', yaxs = 'i')
 #Plot filler data: Both no floods
@@ -249,7 +248,7 @@ lm1950 = lm(MX[MX$YEAR >= 1950 & MX$YEAR <= 1967, 'BeltSum'] ~ MX[MX$YEAR >= 195
 lmPD = lm(MX[MX$YEAR >= 1971, 'BeltSum'] ~ MX[MX$YEAR >= 1971, 'YEAR'])
 
 #Regression diagnostic plots - concerns about heteroskedasticity and normality. Showing one example.
-plot(lm1900)
+#plot(lm1900)
 
 #Regression diagnostics plots
 png('BeltaosRegressionDiagnostics.png', res = 300, units = 'in', width = 8, height = 6)
@@ -265,7 +264,8 @@ plot(lmPD$fitted.values, lmPD$residuals, type = 'p', xlim = c(13,17), ylim = c(-
 lines(c(10,20), c(0,0), lty = 2)
 dev.off()
 
-png('BeltosRegressionCheck.png', res = 300, units = 'in', width = 7, height = 7)
+#Plot regression lines in Beltaos' study
+#png('BeltosRegressionCheck.png', res = 300, units = 'in', width = 7, height = 7)
 par(mar = c(4,5,3,1), xaxs = 'i', yaxs = 'i')
 plot(MX$YEAR, MX$BeltSum, 
      ylim = c(-1,23), xlim = c(1900, max(MX$YEAR)),
@@ -283,7 +283,7 @@ abline(lm1940, col = 'purple')
 abline(lm1950, col = 'pink')
 abline(lmPD, col = 'black')
 
-dev.off()
+#dev.off()
 
 #Evaluation of PDO and Floods----
 plot(seq(1856, 1990,1), PDO_SST_SLP$PDO_SST, type = 'o', pch = 16, col = 'red',
@@ -301,5 +301,159 @@ plot(x = MX$YEAR[MX$Floods23only == 1], y = rep(0.5,length(MX$YEAR[MX$Floods23on
      xlab = '', ylab = '', main = '', axes = FALSE)
 
 #AR Model----
+#Using all data before dam in 1967
+ar1_1826 = ar(MX$Floods23only[MX$YEAR <= 1967], aic = FALSE, order.max = 1)
+ar5_1826 = ar(MX$Floods23only[MX$YEAR <= 1967], aic = TRUE) #5th order is best AIC
+
+#Plot AR(5) model with prediction intervals for post-dam years
+plot(x = MX$YEAR[MX$YEAR <= 1967], MX$Floods23only[MX$YEAR <= 1967], ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = 'Year', ylab = 'Flood Indicator (1 = Flood, 0 = No Flood)', cex.axis = 1.5, cex.lab = 1.5)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR <= 1967], MX$Floods23only[MX$YEAR <= 1967] + ar5_1826$resid, type = 'l', col = 'blue', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR >= 1971], MX$Floods23only[MX$YEAR >= 1971], ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967], predict(ar5_1826, n.ahead = (2018 - 1967))$pred + 2*predict(ar5_1826, n.ahead = (2018 - 1967))$se, type = 'l', lty = 2, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967], predict(ar5_1826, n.ahead = (2018 - 1967))$pred - 2*predict(ar5_1826, n.ahead = (2018 - 1967))$se, type = 'l', lty = 2, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967], predict(ar5_1826, n.ahead = (2018 - 1967))$pred, type = 'l', lty = 1, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+
+#Using data 1900 - 1967
+#Using all data before dam in 1967
+ar1_1900 = ar(MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1900], aic = FALSE, order.max = 1)
+ar0_1900 = ar(MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1900], aic = TRUE) #0 order is best AIC...
+
+#Plot AR model with prediction intervals for post-dam years
+plot(x = MX$YEAR[MX$YEAR <= 1967 & MX$YEAR >= 1900], MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1900], ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = 'Year', ylab = 'Flood Indicator (1 = Flood, 0 = No Flood)', cex.axis = 1.5, cex.lab = 1.5)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR <= 1967 & MX$YEAR >= 1900], MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1900] + ar0_1900$resid, type = 'l', col = 'blue', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR >= 1971 & MX$YEAR >= 1900], MX$Floods23only[MX$YEAR >= 1971 & MX$YEAR >= 1900], ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967 & MX$YEAR >= 1900], predict(ar0_1900, n.ahead = (2018 - 1967))$pred + 2*predict(ar0_1900, n.ahead = (2018 - 1967))$se, type = 'l', lty = 2, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967 & MX$YEAR >= 1900], predict(ar0_1900, n.ahead = (2018 - 1967))$pred - 2*predict(ar0_1900, n.ahead = (2018 - 1967))$se, type = 'l', lty = 2, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967 & MX$YEAR >= 1900], predict(ar0_1900, n.ahead = (2018 - 1967))$pred, type = 'l', lty = 1, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+
+#Using data 1940 - 1967
+#Using all data before dam in 1967
+ar1_1940 = ar(MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1940], aic = FALSE, order.max = 1)
+ar0_1940 = ar(MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1940], aic = TRUE) #0 order is best AIC...
+
+#Plot AR model with prediction intervals for post-dam years
+plot(x = MX$YEAR[MX$YEAR <= 1967 & MX$YEAR >= 1940], MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1940], ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = 'Year', ylab = 'Flood Indicator (1 = Flood, 0 = No Flood)', cex.axis = 1.5, cex.lab = 1.5)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR <= 1967 & MX$YEAR >= 1940], MX$Floods23only[MX$YEAR <= 1967 & MX$YEAR >= 1940] + ar0_1940$resid, type = 'l', col = 'blue', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR >= 1971 & MX$YEAR >= 1940], MX$Floods23only[MX$YEAR >= 1971 & MX$YEAR >= 1940], ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967 & MX$YEAR >= 1940], predict(ar0_1940, n.ahead = (2018 - 1967))$pred + 2*predict(ar0_1940, n.ahead = (2018 - 1967))$se, type = 'l', lty = 2, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967 & MX$YEAR >= 1940], predict(ar0_1940, n.ahead = (2018 - 1967))$pred - 2*predict(ar0_1940, n.ahead = (2018 - 1967))$se, type = 'l', lty = 2, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+par(new = TRUE)
+plot(x = MX$YEAR[MX$YEAR > 1967 & MX$YEAR >= 1940], predict(ar0_1940, n.ahead = (2018 - 1967))$pred, type = 'l', lty = 1, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+
+
+#Using the regressions, make synthetic predictions for post-dam years.
+#NOTE: All models are non-stationary, so this is not a good method to use. proceed down to bootstrapping.
+temp = ar5_1826
+#Set differencing and MA components to 0
+temp$order = c(temp$order, 0, 0)
+#Simulate 50 years many times
+mat = replicate(n = 10000, expr = arima.sim(model = temp, n = 51))
+#Plot the median of those years, and in grey plot all of the years. median should be close to mean
+#par(new = TRUE)
+#matplot(x = MX$YEAR[MX$YEAR > 1967], y = mat, type = 'l', lty = 1, col = adjustcolor('black', alpha=0.003), ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+#par(new = TRUE)
+#matplot(x = MX$YEAR[MX$YEAR > 1967], y = apply(X = mat, MARGIN = 1, FUN = median), type = 'l', lty = 1, col = 'red', ylim = c(-1,2.5), xlim = c(1820, 2020), xlab = '', ylab = '', axes = FALSE)
+
+#Use these synthetic predictions to compute how unusual the observed recrod is.
+#NOTE: All models are non-stationary, so this is not a good method to use. proceed down to bootstrapping.
 
 #Block Bootstrapping----
+#Bootstrap empirical records using only the years pre-dam construction to see how unusual the post-dam years are
+#Size of the bootstrap blocks. 5 is used because AR(5) model had lowest AIC
+block = 5
+#Number of blocks to sample per replicate. Want all the years of record from post-dam to present to be bootstrapped, rounded up.
+numBlk = round((MX$YEAR[nrow(MX)] - 1970)/block,0)
+#Valid years to draw blocks from pre-dam record.
+sampYrs = MX$YEAR[MX$YEAR <= (1967 - block + 1)]
+#Set random seed for reproducibility - using today's date
+set.seed(6818)
+#Draw bootstrapped block samples starting at these years.
+blkStart = replicate(expr = round(runif(n = numBlk, min = sampYrs[1], max = sampYrs[length(sampYrs)])), n = 10000)
+#Extract the flood records using the years in the block samples. Order matters.
+FloodMat = matrix(NA, nrow = nrow(blkStart)*block, ncol = ncol(blkStart))
+for (i in 1:ncol(blkStart)){
+  for (j in 1:nrow(blkStart)){
+    FloodMat[seq(((j-1)*block + 1),((j-1)*block+5),1),i] = MX$Floods23only[MX$YEAR %in% c(blkStart[j,i] + seq(0,4,1))]
+  }
+}
+rm(i,j)
+#Compute cumulative sums
+FldSumMat = apply(X = FloodMat, MARGIN = 2, FUN = cumsum)
+
+#Plot the bootstrapped samples of the flood record on the cumulative chart as lines
+#Figure out the y limit upper bound based on the maximum number of floods observed in 50 years:
+upY = max(apply(X = FloodMat, MARGIN = 2, FUN = sum)) + MX$FldSum[MX$YEAR == 1971]
+
+#As a png file the graphic takes a long time to render. Use PDF instead.
+# png('Block5BootstrappedCumulativeFloods.png', res = 300, units = 'in', width = 6, height = 6)
+# par(mar = c(5,5,3,1), xaxs = 'i', yaxs = 'i')
+# #Plot observed data pre-dam only as a line(? should this show data points?)
+# plot(MX$YEAR[MX$YEAR <= 1971], MX$FldSum[MX$YEAR <= 1971], type = 'l', lwd = 2,
+#      ylim = c(-6,upY), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+#      xlab = '', ylab = '', axes = FALSE)
+# 
+# #Plot synthetic records
+# par(new = TRUE)
+# matplot(x = c(MX$YEAR[MX$YEAR >= 1971], 2019,2020), y = FldSumMat+MX$FldSum[MX$YEAR == 1970], col = adjustcolor('black', alpha=0.01), type = 'l', lwd = 1, lty = 1,
+#      ylim = c(-6,upY), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+#      xlab = 'Year', ylab = 'Cumulative Number of Floods Since 1888',
+#      cex.axis = 1.5, cex.lab = 1.5)
+# 
+# #Plot observed flood record in red
+# par(new = TRUE)
+# plot(MX$YEAR[MX$YEAR >= 1971], MX$FldSum[MX$YEAR >= 1971], type = 'l', lwd = 1, col = 'red',
+#      ylim = c(-6,upY), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+#      xlab = '', ylab = '', axes = FALSE)
+# 
+# 
+# #Polygon for dam filling years
+# polygon(x = c(1968, 1968, 1971, 1971), y = c(-10, upY, upY, -10), col = 'grey', density = 0, lwd = 2)
+# 
+# minor.tick(nx = 5, ny = 5, tick.ratio = 0.5)
+# legend('topleft', legend = c('Observed Record Pre-Dam', 'Observed Record Post-Dam', 'Bootstrapped Records', 'Reservoir Filling'), pch = c(NA, NA, NA, 22), lty = c(1,1,1,NA), lwd = 1, col = c('black', 'red', adjustcolor('black', alpha=0.01), 'grey'), cex = 1.2)
+# 
+# dev.off()
+
+#PDF figure
+pdf('Block5BootstrappedCumulativeFloods.pdf', width = 6, height = 6)
+par(mar = c(5,5,3,1), xaxs = 'i', yaxs = 'i')
+#Plot observed data pre-dam only as a line(? should this show data points?)
+plot(MX$YEAR[MX$YEAR <= 1971], MX$FldSum[MX$YEAR <= 1971], type = 'l', lwd = 2,
+     ylim = c(-6,upY), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+     xlab = '', ylab = '', axes = FALSE)
+
+#Plot synthetic records
+par(new = TRUE)
+matplot(x = c(MX$YEAR[MX$YEAR >= 1971], 2019,2020), y = FldSumMat+MX$FldSum[MX$YEAR == 1970], col = adjustcolor('black', alpha=0.01), type = 'l', lwd = 1, lty = 1,
+        ylim = c(-6,upY), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+        xlab = 'Year', ylab = 'Cumulative Number of Floods Since 1888', main = 'Bootstrapped Synthetic Records for 1971 - 2018 \n 5-Year Block Sampled from 1826 - 1967',
+        cex.axis = 1.5, cex.lab = 1.5)
+
+#Plot observed flood record in red
+par(new = TRUE)
+plot(MX$YEAR[MX$YEAR >= 1971], MX$FldSum[MX$YEAR >= 1971], type = 'l', lwd = 2, col = 'red',
+     ylim = c(-6,upY), xlim = c(MX$YEAR[1], max(MX$YEAR)),
+     xlab = '', ylab = '', axes = FALSE)
+
+
+#Polygon for dam filling years
+polygon(x = c(1968, 1968, 1971, 1971), y = c(-10, upY, upY, -10), col = 'grey', density = 0, lwd = 2)
+
+minor.tick(nx = 5, ny = 5, tick.ratio = 0.5)
+legend('topleft', legend = c('Observed Record Pre-Dam', 'Observed Record Post-Dam', 'Bootstrapped Records', 'Reservoir Filling'), pch = c(NA, NA, NA, 22), lty = c(1,1,1,NA), lwd = 2, col = c('black', 'red', adjustcolor('black', alpha=0.01), 'grey'), cex = 1.2)
+
+dev.off()
